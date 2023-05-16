e733a62ee01ea6b31b6819b4819cae138f1fae76
Enes Esen <g.enesesen@gmail.com>
2023-05-16 15:20:00 +0300

    update basic

---
+++ basic/HelloWorldAPi/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HelloWorldAPi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldAPi.Controllers
+{
+	[ApiController]
+	[Route("home")]
+	public class HomeController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetMessage()
+		{
+			var result = new ResponseModel()
+			{
+				HttpStatus = 200,
+				Message = "ASP.NET Core Web API"
+			};
+			return Ok(result);
+		}
+	}
+}
+
---
+++ basic/HelloWorldAPi/Models/ResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HelloWorldAPi.Models
+{
+	public class ResponseModel
+	{
+		public int HttpStatus { get; set; }
+		public string Message { get; set; }
+	}
+}
+
