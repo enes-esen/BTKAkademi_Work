94f77b8394274b66eaf3dd43963de539fad10586
Enes Esen <g.enesesen@gmail.com>
2023-05-19 03:28:54 +0300

    update 03:28

---
+++ bsStoreApp/WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+	public class RepositoryContext : DbContext
+	{
+		public DbSet<Book> Books { get; set; }
+	}
+}
+
